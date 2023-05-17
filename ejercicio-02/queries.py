f6f9fefffe3d30ca432f7202c401511e95ce46b3
DenisCuenca <dcbuele@gmail.com>
2023-05-17 18:35:18 -0500

    consultas corregidas

--- ejercicio-02/queries.py
+++ ejercicio-02/queries.py
@@ -12,53 +12,50 @@ session = Session()
 
 
 
-    # Presentar todos los países del continente americano
+# Presentar todos los países del continente americano
 
-# countries = session.query(Country).filter(Country.continent=="AM" or Country.continent=="AS" or Country.continent=="NA" ).all()
-
-# for x in countries:
-#     print("%s" % (x))
-#     print("------")
-
-    # Presentar los países de Asía, ordenados por el atributo Dial.
-
-
-# countries = session.query(Country).filter(Country.continent=="AS" ).order_by(Country.dial).all()
-
-
-
-# for x in countries:
-#     print("%s" % (x))
-#     print("------")
-
-#     # Presentar los lenguajes de cada país.
-
-# countries = session.query(Country).all()
-# for x in countries:
-
-#     print(f"país: {x.name}  lenguajes: {x.languages}")
-
-#     # Presentar los países ordenados por la capital, siempre que el país pertenezca a Europa
-
-# countries = session.query(Country).filter(Country.continent=="EU" ).order_by(Country.capital).all()
-
-
-# for x in countries:
-#     print("%s" % (x))
-#     print("------")
-
-
-#     # Presentar todos los países que tengan en su cadena de nombre de país "uador" o en su cadena de capital "ito".
-
-
-    
-# countries = session.query(Country).filter(Country.continent=="EU" ).order_by(Country.capital).all()
-# print(countries)
-
-countries = session.query(Country).filter(or_(Country.name.like("%uador%"), Country.capital.like("%ito"))).all()
-# print(countries)    
+countries = session.query(Country).filter(Country.continent=="AM" or Country.continent=="AS" or Country.continent=="NA" ).all()
 
 for x in countries:
     print("%s" % (x))
     print("------")
-# docentes = session.query(Docente).all()
+# Presentar los países de Asía, ordenados por el atributo Dial.
+
+
+countries = session.query(Country).filter(Country.continent=="AS" ).order_by(Country.dial).all()
+
+
+
+for x in countries:
+    print("%s" % (x))
+    print("------")
+
+# Presentar los lenguajes de cada país.
+
+countries = session.query(Country).all()
+for x in countries:
+
+    print(f"país: {x.name}  lenguajes: {x.languages}")
+
+# Presentar los países ordenados por la capital, siempre que el país pertenezca a Europa
+
+countries = session.query(Country).filter(Country.continent=="EU" ).order_by(Country.capital).all()
+
+
+for x in countries:
+    print("%s" % (x))
+    print("------")
+
+
+# Presentar todos los países que tengan en su cadena de nombre de país "uador" o en su cadena de capital "ito".
+
+
+    
+
+
+countries = session.query(Country).filter(or_(Country.name.like("%uador%"), Country.capital.like("%ito"))).all()
+
+
+for x in countries:
+    print("%s" % (x))
+    print("------")
